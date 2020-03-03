bb0e84a76890b51778fb965145ea3fd61230325e
Luis Pedreira <luispedreiratome@gmail.com>
2020-03-02 20:41:03 -0600

    submited final result, pre feedback

---
+++ script.js
@@ -0,0 +1,161 @@
+
+// define variables to store quiz score/question number. 
+let score = 0;
+let questionNumber = 0;
+
+// function to generate questions for quiz.
+function generateQuestion(){
+  if (questionNumber < STORE.length){
+    return createQuestion(questionNumber);
+  } else {
+    $('.questionBox').hide();
+    finalScore();
+    $('.questionNumber').text(10);
+  }
+}
+//raise value of score and updates text value.
+function updateScore() {
+  score++;
+  $('.score').text(score);
+}
+
+//raise value of question number and update text.
+function updateQuestionNumber() {
+  questionNumber++;
+  $('.questionNumber').text(questionNumber + 1);
+}
+
+// reset value of question num by 1 and updates the text.
+function resetStats() {
+  score = 0;
+  questionNumber = 0;
+  $('.score').text(0);
+  $('.questionNumber').text(0);
+}
+
+// start the quiz.
+function startQuiz() {
+  $('.altBox').hide();
+  $('.quizBox').on('click', '.beginQuizButton', function (event) {
+    $('.quizBox').hide();
+    $('.questionNumber').text(1);
+    $('.questionBox').show();
+    $('.questionBox').prepend(generateQuestion());
+  });
+}
+
+//submits/checks answer against correct answer in STORE.
+function submitAnswer() {
+  $('.backgroundContainer').on('submit', function (event) {
+    event.preventDefault();
+    $('.altBox').hide();
+    $('.response').show();
+    let selected = $('input:checked');
+    let answer = selected.val();
+    let correct = STORE[questionNumber].correctAnswer;
+    if (answer === correct) {
+      correctAnswer();
+    } else {
+      wrongAnswer();
+    }
+  });
+}
+
+function createQuestion(questionIndex) {
+  let formMaker = $(`<form>
+    <fieldset>
+      <legend class="questionText">${STORE[questionIndex].question}</legend>
+    </fieldset>
+  </form>`)
+
+  let fieldSelector = $(formMaker).find('fieldset');
+
+  STORE[questionIndex].options.forEach(function (answerValue, answerIndex) {
+    $(`<label class="quizSize" for="${answerIndex}">
+        <input class="radio" type="radio" id="${answerIndex}" value="${answerValue}" name="answer" required>
+        <span>${answerValue}</span>
+      </label>
+      `).appendTo(fieldSelector);
+  });
+  $(`<button type="submit" class="submitButton button"> Submit</button > `).appendTo(fieldSelector);
+  return formMaker;
+}
+
+function correctAnswer() {
+  $('.response').html(
+    `<h3>You got it right!</h3>
+    <img src="images/success.jpg" alt="DJ celebrating at a show" class="images">
+      <p class="quizSize">Party on, raver!</p>
+      <button type="button" class="nextButton button">Next</button>`
+  );
+  updateScore();
+}
+
+function wrongAnswer() {
+  $('.response').html(
+    `<h3>Not the right one.</h3>
+    <img src="images/failure.jpg" alt="sad DJ in a room" class="images">
+    <p class="quizSize">It's actually:</p>
+    <p class="quizSize">${STORE[questionNumber].correctAnswer}</p>
+    <button type="button" class="nextButton button">Next</button>`
+  );
+}
+
+function nextQuestion() {
+  $('.backgroundContainer').on('click', '.nextButton', function (event) {
+    $('.altBox').hide();
+    $('.questionBox').show();
+    updateQuestionNumber();
+    $('.questionBox form').replaceWith(generateQuestion());
+  });
+}
+
+function finalScore() {
+  $('.final').show();
+
+  const pass = [
+    'You passed!',
+    'images/win.jpg',
+    'big edm festival',
+    'A true EDM master!'
+  ];
+
+  const fail = [
+    'Oof, not quite there chief',
+    'images/loss.jpg',
+    'empty EDM show',
+    'Try again?'
+  ];
+
+  if (score >= 7) {
+    array = pass; 
+  } else {
+    array = fail;
+  }
+  return $('.final').html(
+    `<h3>${array[0]}</h3>
+      <img src="${array[1]}" alt="${array[2]}" class="images">
+        <h3>Your score is ${score} / 10</h3>
+        <p class="quizSize">${array[3]}</p>
+        <button type="submit" class="restartButton button">Restart</button>`
+  );
+}
+
+function restartQuiz() {
+  $('.final').on('click', '.restartButton', function (event) {
+    event.preventDefault();
+    resetStats();
+    $('.altBox').hide();
+    $('.quizBox').show();
+  });
+}
+
+function makeQuiz() {
+  startQuiz();
+  generateQuestion();
+  submitAnswer();
+  nextQuestion();
+  restartQuiz();
+}
+
+$(makeQuiz);
---
+++ store.js
@@ -0,0 +1,106 @@
+const STORE = [
+  {
+    question: "DUBSTEP - Often linked with the pejorative ‘Brostep/Post-Dubstep’ qualifier, what western producer entered the scene in mid 2010, directly influencing the genre’s western appeal and bringing it to the forefront of American popular music at the time?",
+    options: [
+      "Goldie", 
+      "Dillon Francis", 
+      "Skrillex", 
+      "DJ Fresh"
+    ],
+    correctAnswer: "Skrillex"
+  },
+  //2
+  {
+    question: "DNB - What is the most used drum sample in over 80% of contemporary drum and bass tracks?",
+    options: [
+      "Amen, Brother - The Winstons",
+      "Funky Drummer - James Brown", 
+      "Do The Do - Kurtis Blow",
+      "The Harlem Shake - Baauer"
+    ],
+    correctAnswer: "Amen, Brother - The Winstons"
+  },
+  //3
+  {
+    question: "TRANCE - Generally regarded as a fairly underground genre, this subgenre of trance is known for its energetic, mesmerizing, layered sounds, strung along around 6-10 minutes long, with samples relating to drugs, metaphysics, and human consciousness.",
+    options: [
+      "Goa Trance", 
+      "Psy Trance", 
+      "Industrial Trance", 
+      "Subliminal Rhythm"
+    ],
+    correctAnswer: "Goa Trance"
+  },
+  //4
+  {
+    question: "HOUSE - By all accounts the bread and butter of the genre, this beat is uniform and easily audible, allowing producers to find easy ways to tweak and mix it around with a lot of other genres/samples. Inherited from the age of Disco.", 
+    options: ["Four-to-the-Floor", "Mambo No. 15", "Pulse Beat n Slide", "Hardstyle"],
+    correctAnswer: "Four-to-the-Floor"
+  },
+  //5
+  {
+    question: "HARDSTYLE - This style of dance is largely linked with the genre, known for it’s almost physics-defying sliding visuals, often accompanied with colorful ankle-length Tripp Pants. It’s performers have spread it from The Netherlands to Australia.",
+    options: [
+      "X-Outing", 
+      "2-Step", 
+      "Melbourne Shuffle", 
+      "Jumpstyle"
+    ],
+    correctAnswer: "Melbourne Shuffle"
+  },
+  //6
+  {
+    question: "Electro Swing - For a genre filled with movements reminiscent of vintage swing and jazz, this song in particular picked up much mainstream popularity by remixing an old 1950s neapolitan song and bringing it to the dance floor of 2011.",
+    options: [
+      "We No Speak Americano - Yolanda Be Cool",
+      "Sometimes I Get a Good Feeling - Pretty Lights", 
+      "City to City - Zomboy", 
+      "Stomp On The Ground - Italobrothers"
+    ],
+    correctAnswer: "We No Speak Americano - Yolanda Be Cool"
+  },
+  //7
+  {
+    question: 'GENRE - Characterized by it’s hard, repetitive sounds, this drum machine-driven genre came out of the post-motown era concrete jungle of Detroit in the late 80s, illuminating Americans to a sound that up until this point had only found an audience in Europe.',
+    options: [
+      "Electro", 
+      "Post Dub", 
+      "Minimal", 
+      "Techno"
+    ],
+    correctAnswer: "Techno"
+  },
+  //8
+  {
+    question: "Industrial - This subculture arose around the genre, finding common ground with it’s heavy beats, dystopian aesthetic and grim-dark futuristic imagery. Extravagant hairstyles, black PVC and oxygen masks span the room, as it’s inhabitants bob, pop and shift to the music in abandoned warehouses and dimly lit underground clubs.", 
+    options: [
+      "Electropunks", 
+      "Metalheads", 
+      "Cybergoths", 
+      "Technovampires"
+    ],
+    correctAnswer: "Cybergoths"
+  },
+  //9
+  {
+    question: "GENRE - With its roots firmly in Jamaican/Caribbean expatriots finding a place for self-expression, this genre instead boasts heavy syncopation and breakbeats, often samplings drum patterns found widely in funk and jazz. A distinct departure from the Eurodisco, Techno and Hardcore of the 80s and a precursor to Drum and Bass",
+    options: [
+      "Future Funk", 
+      "Retro Dub Style", 
+      "Jungle", 
+      "Darkstomp"
+    ], 
+    correctAnswer: "Jungle"
+  },
+  //10
+  {
+    question: "Moombahton - As bastardized a genre as any, Moombahton sought to fuse the elements of -this- genre’s rhythms with house music’s expression and remixability. Influenced by American Hip Hop, Jamaican Dancehall and Spanish Reggae from the early 90s, it hit arose from Puerto Rico’s club scene to early 2000s acclaim.",
+    options: [
+      "Raitru", 
+      "Soca", 
+      "Afrobeat", 
+      "Reggaeton"
+    ],
+    correctAnswer: "Reggaeton"
+  }
+];
