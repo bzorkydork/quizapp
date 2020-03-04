e63a3f06fae2d4cb485628009ce1d764ff61bd1f
Luis Pedreira <luispedreiratome@gmail.com>
2020-03-03 18:37:23 -0600

    Pre-Final Feedback Commit

--- store.js
+++ store.js
@@ -1,6 +1,6 @@
 const STORE = [
   {
-    question: "DUBSTEP - Often linked with the pejorative ‘Brostep/Post-Dubstep’ qualifier, what western producer entered the scene in mid 2010, directly influencing the genre’s western appeal and bringing it to the forefront of American popular music at the time?",
+    question: "DUBSTEP - What western producer entered the scene in mid 2010, directly influencing the genre’s western appeal and bringing it to the forefront of American popular music at the time?",
     options: [
       "Goldie", 
       "Dillon Francis", 
@@ -22,24 +22,24 @@ const STORE = [
   },
   //3
   {
-    question: "TRANCE - Generally regarded as a fairly underground genre, this subgenre of trance is known for its energetic, mesmerizing, layered sounds, strung along around 6-10 minutes long, with samples relating to drugs, metaphysics, and human consciousness.",
+    question: "TRANCE - This subgenre of trance is known for its energetic, mesmerizing, layered sounds, with samples relating to drugs, metaphysics, and human consciousness.",
     options: [
       "Goa Trance", 
-      "Psy Trance", 
+      "Psytrance", 
       "Industrial Trance", 
       "Subliminal Rhythm"
     ],
-    correctAnswer: "Goa Trance"
+    correctAnswer: "Psytrance"
   },
   //4
   {
-    question: "HOUSE - By all accounts the bread and butter of the genre, this beat is uniform and easily audible, allowing producers to find easy ways to tweak and mix it around with a lot of other genres/samples. Inherited from the age of Disco.", 
+    question: "HOUSE - This beat is uniform and easily audible, allowing producers to tweak and mix it around with genres/samples. Inherited from the age of Disco.", 
     options: ["Four-to-the-Floor", "Mambo No. 15", "Pulse Beat n Slide", "Hardstyle"],
     correctAnswer: "Four-to-the-Floor"
   },
   //5
   {
-    question: "HARDSTYLE - This style of dance is largely linked with the genre, known for it’s almost physics-defying sliding visuals, often accompanied with colorful ankle-length Tripp Pants. It’s performers have spread it from The Netherlands to Australia.",
+    question: "HARDSTYLE - This style of dance is largely linked with the genre, known for it’s almost physics-defying sliding visuals, often accompanied with colorful ankle-length Tripp Pants.",
     options: [
       "X-Outing", 
       "2-Step", 
@@ -50,7 +50,7 @@ const STORE = [
   },
   //6
   {
-    question: "Electro Swing - For a genre filled with movements reminiscent of vintage swing and jazz, this song in particular picked up much mainstream popularity by remixing an old 1950s neapolitan song and bringing it to the dance floor of 2011.",
+    question: "Electro Swing - This song in particular picked up much mainstream popularity by remixing an old 1950s neapolitan song and bringing it to the dance floor of 2011.",
     options: [
       "We No Speak Americano - Yolanda Be Cool",
       "Sometimes I Get a Good Feeling - Pretty Lights", 
@@ -61,7 +61,7 @@ const STORE = [
   },
   //7
   {
-    question: 'GENRE - Characterized by it’s hard, repetitive sounds, this drum machine-driven genre came out of the post-motown era concrete jungle of Detroit in the late 80s, illuminating Americans to a sound that up until this point had only found an audience in Europe.',
+    question: "GENRE - Characterized by it’s hard, repetitive sounds, this drum machine-driven genre came out of the post-motown era concrete jungle of Detroit in the late 80s",
     options: [
       "Electro", 
       "Post Dub", 
@@ -72,7 +72,7 @@ const STORE = [
   },
   //8
   {
-    question: "Industrial - This subculture arose around the genre, finding common ground with it’s heavy beats, dystopian aesthetic and grim-dark futuristic imagery. Extravagant hairstyles, black PVC and oxygen masks span the room, as it’s inhabitants bob, pop and shift to the music in abandoned warehouses and dimly lit underground clubs.", 
+    question: "Industrial - This subculture arose around the genre. Extravagant hairstyles, black PVC and oxygen masks span the room, as it’s inhabitants bob and weave to the music in abandoned warehouses.", 
     options: [
       "Electropunks", 
       "Metalheads", 
@@ -83,7 +83,7 @@ const STORE = [
   },
   //9
   {
-    question: "GENRE - With its roots firmly in Jamaican/Caribbean expatriots finding a place for self-expression, this genre instead boasts heavy syncopation and breakbeats, often samplings drum patterns found widely in funk and jazz. A distinct departure from the Eurodisco, Techno and Hardcore of the 80s and a precursor to Drum and Bass",
+    question: "GENRE - With its roots with Jamaican/Caribbean expats, this genre boasts heavy syncopation and breakbeats, samplings drum patterns found in funk and jazz. A precursor to Drum and Bass",
     options: [
       "Future Funk", 
       "Retro Dub Style", 
@@ -94,7 +94,7 @@ const STORE = [
   },
   //10
   {
-    question: "Moombahton - As bastardized a genre as any, Moombahton sought to fuse the elements of -this- genre’s rhythms with house music’s expression and remixability. Influenced by American Hip Hop, Jamaican Dancehall and Spanish Reggae from the early 90s, it hit arose from Puerto Rico’s club scene to early 2000s acclaim.",
+    question: "Moombahton - Moombahton sought to fuse the elements of -this- genre’s rhythms with house music’s expression. It arose from Puerto Rico’s club scene to early 2000s acclaim.",
     options: [
       "Raitru", 
       "Soca", 
